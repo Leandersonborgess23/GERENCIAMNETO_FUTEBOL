880de96d92cdcfaec3900858eeb90c5a2d4e526e
LBXS_36 <borges.leanderson@escolar.ifrn.edu.br>
2025-10-02 11:41:57 -0300

    segurança config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import secrets
+from datetime import timedelta
 
 
 class Config:
     SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///gerenciamento_futebol.db'
+    SESSION_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_SAMESITE="Lax"
+    SESSION_COOKIE_SECURE=False
+    REMEMBER_COOKIE_DURATION=timedelta(days=7)
